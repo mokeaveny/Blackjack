40bf2c6a38382272a32c4d3a3427251659b00744
Michael Keaveny <michaelkeaveny0@gmail.com>
2021-03-21 00:10:54 +0000

    Created a Card class. As well as a deck of cards that contain all 56 card objects with the correct value and suit

---
+++ Blackjack/Card.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blackjack
+{
+    class Card
+    {
+        // The value of the card. This will be 1 - 10 for numbered cards.
+        // 11 - Jack, 12 - Queen, 13 - King, 14 - Ace
+        private int value;
+        private Suit suit;
+        public enum Suit
+        {
+            Clubs, Spades, Hearts, Diamonds
+        }
+
+        public int getValue()
+        {
+            return this.value;
+        }
+
+        public void setValue(int value)
+        {
+            this.value = value;
+        }
+
+        public Suit getSuit()
+        {
+            return this.suit;
+        }
+
+        public Card(int value, Suit suit)
+        {
+            this.value = value;
+            this.suit = suit;
+        }
+
+        public override string ToString()
+        {
+            string output = "";
+            if (value > 10)
+            {
+                switch (value)
+                {
+                    case 11:
+                        output += "Value: Jack";
+                        break;
+                    case 12:
+                        output += "Value: Queen";
+                        break;
+                    case 13:
+                        output += "Value: King";
+                        break;
+                    case 14:
+                        output += "Value: Ace";
+                        break;
+                }
+            }
+            else
+            {
+                output += "Value: " + value.ToString();
+            }
+
+            output += " Suit: " + this.getSuit().ToString();
+
+            return output;
+        }
+    }
+}
--- Blackjack/Program.cs
+++ Blackjack/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Blackjack
 {
@@ -6,7 +7,23 @@ namespace Blackjack
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // 4 Suits and 14 values for each suit. 4 * 14 = 56. Therefore an array that contains 56 card objects.
+            List<Card> deckOfCards = new List<Card>();
+
+            // Iterates over every enum suit value.
+            foreach(Card.Suit suit in Enum.GetValues(typeof(Card.Suit)))
+            {
+                // Iterate from 1 to 14.
+                for(int i = 1; i < 15; i++)
+                {
+                    deckOfCards.Add(new Card(i, suit));
+                }
+            }
+
+            foreach(Card card in deckOfCards)
+            {
+                Console.WriteLine(card.ToString());
+            }
         }
     }
 }
